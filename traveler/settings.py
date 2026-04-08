ca4f1f9a6672ead9d8632e0ed32228a57ec5c141
Arjun1Alpha <arjun.thakur@thealphagency.com>
2026-04-08 13:27:16 +0530

    add

--- traveler/settings.py
+++ traveler/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-ys6^xp21q=4b753^o+j)!9cze17=ao0yv5j!pk1=8kohkk8@gi
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['traveller.arjunthakur.in']
 
 
 # Application definition
